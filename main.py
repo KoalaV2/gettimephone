49b827d28185a6d5fe0eae4c72fc5be059a9c9cc
KoalaV2 <theo.johanson@gmail.com>
2020-12-12 16:21:20 +0100

    fixed

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-
+import requests
+import urllib3.request
+import time
+from bs4 import BeautifulSoup
 
 def main():
-    name = 'gettimescrape'
-    URL = ["https://gettime.ga"
+    year = input("Which year are you in? \n :")
+    url = f'https://gettime.ga/schema/{year}'
 
-    print("Hello world")
+    response = requests.get(url)
+    print(response,'\n')
 
+    soup = BeautifulSoup(response.text,"html.parser")
+    result = soup.findAll('img')[0]
 
+    print(result,'\n')
+    
+    link = result['src']
+
+    print("Image link is: https://gettime.ga"+link,'\n')
 if __name__=="__main__":
     main()
