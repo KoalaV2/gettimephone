b8fce6b64bd20c4def59c4f8c96460f8ea773ef2
KoalaV2 <theo.johanson@gmail.com>
2020-12-12 16:36:23 +0100

    downlads image now

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import urllib3.request
 import time
 from bs4 import BeautifulSoup
-
+import shutil
 def main():
     year = input("Which year are you in? \n :")
     url = f'https://gettime.ga/schema/{year}'
@@ -19,5 +19,11 @@ def main():
     link = result['src']
 
     print("Image link is: https://gettime.ga"+link,'\n')
+    url_image = f"https://gettime.ga{link}"
+    response_image = requests.get(url_image, stream=True)
+    with open(f'{year}.png', 'wb') as out_file:
+        shutil.copyfileobj(response_image.raw,out_file)
+    del response_image
+    print(url_image)
 if __name__=="__main__":
     main()
