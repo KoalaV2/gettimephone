0856a03c14c8022d7fbe185ab39c49a447217308
KoalaV2 <theo.johanson@gmail.com>
2020-12-13 01:59:46 +0100

    stuff

---
+++ ios/gettimephone/gettimephone/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  gettimephone
+//
+//  Created by Theodor Johanson on 2020-12-13.
+//
+
+import SwiftUI
+import SwiftSoup
+
+
+func webscrape(){
+        do {
+            if let url = URL(string: "https://gettime.ga/schema/20_el_a") {
+                do {
+//                    let className = "schemaWeekDesktop"
+                    let contents = try String(contentsOf: url)
+                    //print(contents)
+                    let doc: Document = try SwiftSoup.parse(contents)
+                    let schema: Element = try doc.select("img[src$=.png]").first()!
+                    let schema_image = try schema.text()
+                    print(schema_image)
+                } catch {
+                    print("Contents could not be loaded")
+                }
+            } else {
+                print("Url was bad!")
+            }
+        }
+}
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        Button(action: {
+            webscrape()
+        }) {
+            Text("Press me!")
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/gettimephone/gettimephone/gettimephoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  gettimephoneApp.swift
+//  gettimephone
+//
+//  Created by Theodor Johanson on 2020-12-13.
+//
+
+import SwiftUI
+
+@main
+struct gettimephoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
